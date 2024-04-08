5b4538e0c423f1f20862edd8792413c71754c31b
DuckyKay <bloodyduckz@gmail.com>
2024-04-08 02:57:07 -0700

    Implement Parser

--- src/main/java/com/example/demo/Parser.java
+++ src/main/java/com/example/demo/Parser.java
@@ -7,30 +7,122 @@ import java.io.*;
 public class Parser {
     private Document doc;
 
-    public Parser(String filePath) {
+    public Parser(String xmlFilePath, String csvFilePath) {
+        
         try {
-            File inputFile = new File(filePath);
-            DocumentBuilderFactory dbFactory = DocumentBuilderFactory.newInstance();
-            DocumentBuilder dBuilder = dbFactory.newDocumentBuilder();
-            doc = dBuilder.parse(inputFile);
-            doc.getDocumentElement().normalize();
+            if (xmlFilePath != null) {
+                parseXML(xmlFilePath);
+            }
+
+            if (csvFilePath != null) {
+                parseCSV(csvFilePath);
+            }
         } catch (Exception e) {
             e.printStackTrace();
         }
-
-
-
-
-
-
-
-
-
-
         
     }
-     // Parse threads, ramp time, duration from JMX file
-    public void parseThreadGroup() {
+
+    private void parseXML(String xmlFilePath) throws Exception {
+        File xmlFile = new File(xmlFilePath);
+        DocumentBuilderFactory xmlFactory = DocumentBuilderFactory.newInstance();
+        DocumentBuilder xmlBuilder = xmlFactory.newDocumentBuilder();
+        doc = xmlBuilder.parse(xmlFile);
+        doc.getDocumentElement().normalize();
+    }
+
+    public CSVProperties parseCSV(String csvFilePath) {
+        int totalResponseTime = 0;
+        int successCount = 0;
+        int successRate;
+
+        try {
+            File csvFile = new File(csvFilePath);
+            BufferedReader br = new BufferedReader(new FileReader(csvFile));
+            String line;
+            int lineCount = 0; 
+            boolean skipHeader = true; // Skip the header line
+
+            while ((line = br.readLine()) != null) {
+                if (skipHeader) {
+                    skipHeader = false;
+                    continue; 
+                }
+
+                 // Read every third line
+                if (lineCount % 3 == 0) { 
+                    String[] data = line.split(","); 
+                    long responseTime = Long.parseLong(data[1]);
+                    totalResponseTime += responseTime;
+                    if (data[7].equals("true")) {
+                        successCount++;
+                    } 
+                    lineCount++;
+                }
+                lineCount++;
+            }
+            br.close();
+
+            int testedLines = lineCount / 3;
+            double average = (double) totalResponseTime / testedLines;
+            successRate = (successCount / testedLines) * 100;
+
+            return new CSVProperties(average, successRate);
+
+        } catch (Exception e) {
+            e.printStackTrace();
+            return null;
+        }
+    }
+
+    public static class CSVProperties {
+        double avgResponseTime = 0;
+        int successRate = 0;
+
+        public CSVProperties(double avgResponseTime, int successRate) {
+            this.avgResponseTime = avgResponseTime;
+            this.successRate = successRate;
+        }
+
+        public double getAvgResponseTime() {
+            return avgResponseTime;
+        }
+
+        public int getSuccessRate() {
+            return successRate;
+        }
+
+    }
+
+    // inner class to retrieve JMX properties
+    public static class JMXProperties {
+        private int numThreads;
+        private int rampTime;
+        private long duration;
+
+        // Constructor
+        public JMXProperties(int numThreads, int rampTime, long duration) {
+            this.numThreads = numThreads;
+            this.rampTime = rampTime;
+            this.duration = duration;
+        }
+
+        // Getters
+        public int getNumThreads() {
+            return numThreads;
+        }
+
+        public int getRampTime() {
+            return rampTime;
+        }
+
+        public long getDuration() {
+            return duration;
+        }
+    }    
+
+    // Parse threads, ramp time, duration from JMX file
+    public JMXProperties parseJMXProperties() {
         try {
             NodeList threadGroupNodeList = doc.getElementsByTagName("ThreadGroup");
             if (threadGroupNodeList.getLength() > 0) {
@@ -39,39 +131,61 @@ public class Parser {
                 int rampTime = Integer.parseInt(threadGroupElement.getElementsByTagName("intProp").item(1).getTextContent());
                 long duration = Long.parseLong(threadGroupElement.getElementsByTagName("longProp").item(0).getTextContent());
 
-                System.out.println("Number of Threads: " + numThreads);
-                System.out.println("Ramp Time: " + rampTime);
-                System.out.println("Duration: " + duration);
+                return new JMXProperties(numThreads, rampTime, duration);
             } else {
                 System.err.println("ThreadGroup element not found.");
+                return null;
             }
         } catch (Exception e) {
             e.printStackTrace();
+            return null;
         }
     }
 
-    // Parse domain (URL) from .JMX
-    public void parseHTTPSamplerProxy() {
+    public String getPropertyFromHttpSampler(String propertyName) {
         try {
             NodeList httpSamplerNodeList = doc.getElementsByTagName("HTTPSamplerProxy");
             if (httpSamplerNodeList.getLength() > 0) {
                 Element httpSamplerElement = (Element) httpSamplerNodeList.item(0);
-                String domain = httpSamplerElement.getElementsByTagName("stringProp").item(0).getTextContent();
-
-                System.out.println("Domain: " + domain);
+                if ("testname".equals(propertyName)) {
+                    return httpSamplerElement.getAttribute(propertyName);
+                } else {
+                    return httpSamplerElement.getElementsByTagName("stringProp").item(0).getTextContent();
+                }
             } else {
                 System.err.println("HTTPSamplerProxy element not found.");
+                return null;
             }
         } catch (Exception e) {
             e.printStackTrace();
+            return null;
         }
     }
-
-    // You can add more methods to parse other parts of the XML or CSV files
+    
+    public String parseDomain() {
+        return getPropertyFromHttpSampler("HTTPSampler.domain");
+    }
+    
+    public String parseTestName() {
+        return getPropertyFromHttpSampler("testname");
+    }
 
     public static void main(String[] args) {
-        Parser parser = new Parser("nba.jmx"); // Is there only 1 JMX? I am going to just use the NBA one
-        parser.parseThreadGroup();
-        parser.parseHTTPSamplerProxy();
+        Parser p = new Parser("nba.jmx", "results.csv"); 
+        JMXProperties j = p.parseJMXProperties();
+        CSVProperties c = p.parseCSV("results.csv");
+        System.out.println("Duration: " + j.getDuration());
+        System.out.println("Thread Num: " + j.getNumThreads());
+        System.out.println("Ramp Time: " + j.getRampTime());
+        System.out.println("Domain: " + p.parseDomain());
+        System.out.println("Avg Response Time: " + c.getAvgResponseTime());
+        System.out.println("Test Name: " + p.parseTestName());
+        System.out.println("Success Rate: " + c.getSuccessRate());
+
+
+        /*
+         *  import com.example.demo.Parser.CSVProperties;
+            import com.example.demo.Parser.JMXProperties;
+         */
     }
 }
